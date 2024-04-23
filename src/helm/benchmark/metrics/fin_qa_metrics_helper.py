72307ebebd8cba66d92882b0388f4d993f86d537
Yifan Mai <yifan@cs.stanford.edu>
2024-04-23 09:59:41 -0700

    Add license

--- src/helm/benchmark/metrics/fin_qa_metrics_helper.py
+++ src/helm/benchmark/metrics/fin_qa_metrics_helper.py
@@ -1,6 +1,34 @@
 # type: ignore
 # flake8: noqa
 # fmt: off
+"""Evaluation metrics for FinQA.
+
+This evaluation code is reproduced from the following URL with the following license.
+
+URL: https://github.com/czyssrs/FinQA/blob/0f16e2867befa6840783e58be38c9efb9229d742/code/evaluate/evaluate.py
+
+License: MIT License
+
+Copyright (c) 2021 Zhiyu Chen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 from sympy import simplify
 
 
